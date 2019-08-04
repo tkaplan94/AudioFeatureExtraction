2120a2015d061bd424ff9c8f5ab078b1d11ba693
Tyler <tkaplan94@yahoo.com>
2019-08-03 19:53:00 -0700

    Added STFT

--- Audio Feature Extraction.py
+++ Audio Feature Extraction.py
@@ -1,12 +1,15 @@
+import matplotlib.pyplot as plt
 import librosa.display
+import os
+import glob
 
 
 def find_zerocrossings(sample):
     print("Zero Crossings")
     print("-----------------------------------------------")
     zero_crossings = librosa.zero_crossings(sample, pad=False)
-    print("type:  ", end='')
-    print(type(zero_crossings))
+    print("shape: ", end='')
+    print(zero_crossings.shape)
     print("print: ", end='')
     print(zero_crossings)
     print("sum:   ", end='')
@@ -20,29 +23,24 @@ def find_decibels(sample):
     print("Amplitude to dB")
     print("-----------------------------------------------")
     decibels = librosa.amplitude_to_db(sample)
-    print("type:  ", end='')
-    print(type(decibels))
+    print("shape: ", end='')
+    print(decibels.shape)
     print("print: ", end='')
     print(decibels)
     print("sum:   ", end='')
-    print(sum(decibels))
+    print(abs(sum(decibels)))
     print("avg:   ", end='')
-    print(sum(decibels) / len(decibels))
+    print(abs(sum(decibels) / len(decibels)))
     print()
 
 
-def find_tempogram(sample):
-    print("Tempogram")
+def find_stft(sample):
+    print("STFT")
     print("-----------------------------------------------")
-    tempogram = librosa.feature.tempogram(sample)
-    print("type:  ", end='')
-    print(type(tempogram))
-    print("print: ")
-    print(tempogram)
-    print("sum:   ")
-    print(sum(tempogram))
-    print("avg:   ")
-    print(sum(tempogram) / len(tempogram))
+    stft = librosa.stft(sample)
+    print("shape: ", end='')
+    print(stft.shape)
+
     print()
 
 
@@ -50,27 +48,23 @@ def find_fouriertempogram(sample):
     print("Fourier Tempogram")
     print("-----------------------------------------------")
     ftempogram = librosa.feature.fourier_tempogram(sample)
-    print("type:  ", end='')
-    print(type(ftempogram))
-    print("print: ")
-    print(ftempogram)
-    print("sum:   ")
-    print(sum(ftempogram))
-    print("avg:   ")
-    print(sum(ftempogram) / len(ftempogram))
+    print("shape: ", end='')
+    print(ftempogram.shape)
+
     print()
 
+directory =
+data, sampling_rate = librosa.load(directory, sr=44100)
 
-audio_path =
-data, sampling_rate = librosa.load(audio_path, sr=44100)
-
-# DISPLAY: Waveform
-# plt.figure(figsize=(10, 5))
+# # DISPLAY: Waveform
+# fig = plt.figure(figsize=(10, 5))
+# fig.canvas.set_window_title(os.path.basename(audio_path))
 # librosa.display.waveplot(data, sr=sampling_rate)
 # plt.show()
 
-# DISPLAY: Spectrogram
-# plt.figure(figsize=(10, 5))
+# # DISPLAY: Spectrogram
+# fig = plt.figure(figsize=(10, 5))
+# fig.canvas.set_window_title(os.path.basename(audio_path))
 # X = librosa.stft(data)
 # Xdb = librosa.amplitude_to_db(abs(X))
 # librosa.display.specshow(Xdb, sr=sampling_rate, x_axis='time', y_axis='hz')
